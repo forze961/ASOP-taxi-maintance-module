76b3c9ef2507ecf7a51debc0295f95fd5393d91d
Dmitry <lyama961@gmail.com>
2021-12-12 21:02:37 +0200

    ✨ added pwa settings (2)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,11 +30,33 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="uk">
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
+          <meta name="application-name" content="Route taxi's" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="Route taxi's" />
+          <meta name="description" content="Best Route taxi's" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:title" content="Route taxi's" />
+          <meta name="twitter:description" content="Route taxi's" />
+          <meta name="twitter:image" content="icons/android-chrome-192x192.png" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Route taxi's" />
+          <meta property="og:description" content="Route taxi's" />
+          <meta property="og:site_name" content="Route taxi's" />
+          <meta property="og:image" content="/icons/maskable_icon_x192.png" />
         </Head>
         <body>
           <Main />
