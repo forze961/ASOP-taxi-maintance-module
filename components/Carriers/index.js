03c1efd9f591023560af334c84c6286be1e15f05
Taras  Batyuchok <57962357+TarasBatyuchok@users.noreply.github.com>
2023-07-20 21:59:27 +0300

    add table crud operation and have error 401 login

--- components/Carriers/index.js
+++ components/Carriers/index.js
@@ -1,33 +1,31 @@
 /* eslint-disable prefer-const */
 // axios
-import axios from 'axios';
+import axios from "axios";
 // react hooks
-import {useState, useEffect, useCallback} from 'react';
+import { useState, useEffect, useCallback } from "react";
 
-import { forwardRef } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-
-import TableCell from '@material-ui/core/TableCell';
+import { forwardRef } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 // component
-import MaterialTable from 'material-table';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import MaterialTable from "material-table";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 // Icons
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
+import AddBox from "@material-ui/icons/AddBox";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import Check from "@material-ui/icons/Check";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+import Clear from "@material-ui/icons/Clear";
+import DeleteOutline from "@material-ui/icons/DeleteOutline";
+import Edit from "@material-ui/icons/Edit";
+import FilterList from "@material-ui/icons/FilterList";
+import FirstPage from "@material-ui/icons/FirstPage";
+import LastPage from "@material-ui/icons/LastPage";
+import Remove from "@material-ui/icons/Remove";
+import SaveAlt from "@material-ui/icons/SaveAlt";
+import Search from "@material-ui/icons/Search";
+import ViewColumn from "@material-ui/icons/ViewColumn";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -35,26 +33,25 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(3),
-    textAlign: 'left',
-    color: "transparent" ,
+    textAlign: "left",
+    color: "transparent",
   },
 
   table: {
     minWidth: 300,
-    '& .MuiTableCell-root': {
-      borderRight: '1px solid rgba(224, 224, 224, 1)',
+    "& .MuiTableCell-root": {
+      borderRight: "1px solid rgba(224, 224, 224, 1)",
     },
   },
   search: {
-    position: 'relative',
+    position: "relative",
     borderRadius: theme.shape.borderRadius,
-    '&:hover': {
-    },
+    "&:hover": {},
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(1),
-      width: 'auto',
+      width: "auto",
     },
   },
 
@@ -62,24 +59,24 @@ const useStyles = makeStyles((theme) => ({
     minWidth: 120,
   },
   smallTableCell: {
-    maxWidth: '30px',
+    maxWidth: "30px",
   },
   tableHeaderFirst: {
-    backgroundColor: '#FFFBF4',
+    backgroundColor: "#FFFBF4",
   },
   tableHeaderSecond: {
-    backgroundColor: '#FFFBF4',
+    backgroundColor: "#FFFBF4",
   },
   selectTableCell: {
     width: 140,
-    borderLeft: '1px solid rgba(224, 224, 224, 1)',
+    borderLeft: "1px solid rgba(224, 224, 224, 1)",
   },
   selectTableCellEdit: {
     width: 140,
-    borderLeft: '5px solid #ED9137',
+    borderLeft: "5px solid #ED9137",
   },
   bg: {
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
   },
 }));
 
@@ -90,8 +87,8 @@ const createNewData = (obj, rows) => ({
 });
 
 const getCellSplit = (row, name) => {
-  if (row[name] && typeof row[name] === 'string' && row[name].includes('/')) {
-    const splited = row[name].split('/');
+  if (row[name] && typeof row[name] === "string" && row[name].includes("/")) {
+    const splited = row[name].split("/");
 
     return (
       <>
@@ -105,40 +102,37 @@ const getCellSplit = (row, name) => {
   return row[name];
 };
 
-const CustomTableCell = ({
-  row, name, onChange, onChangeDate,
-}) => {
+const CustomTableCell = ({ row, name, onChange, onChangeDate }) => {
   const classes = useStyles();
   const { isEditMode } = row;
   return (
     <TableCell align="center" className={classes.tableCell}>
-      {
-        isEditMode ? (
-            name.includes('date') ? (
-              <KeyboardDatePicker
-                disableToolbar
-                variant="inline"
-                format="yyyy-MM-dd"
-                id="date-picker-inline"
-                label="Оберіть дату"
-                value={row[name]}
-                onChange={(value) => onChangeDate(value, name, row)}
-                autoOk
-                KeyboardButtonProps={{
-                  'aria-label': 'change date',
-                }}
-              />
-            ) : (
-              <Input
-                value={row[name]}
-                name={name}
-                onChange={(e) => onChange(e, row)}
-                className={classes.input}
-              />
-            )
-          )
-          : (getCellSplit(row, name))
-      }
+      {isEditMode ? (
+        name.includes("date") ? (
+          <KeyboardDatePicker
+            disableToolbar
+            variant="inline"
+            format="yyyy-MM-dd"
+            id="date-picker-inline"
+            label="Оберіть дату"
+            value={row[name]}
+            onChange={(value) => onChangeDate(value, name, row)}
+            autoOk
+            KeyboardButtonProps={{
+              "aria-label": "change date",
+            }}
+          />
+        ) : (
+          <Input
+            value={row[name]}
+            name={name}
+            onChange={(e) => onChange(e, row)}
+            className={classes.input}
+          />
+        )
+      ) : (
+        getCellSplit(row, name)
+      )}
     </TableCell>
   );
 };
@@ -155,31 +149,30 @@ export default function Carriers() {
   const [created, setCreated] = useState(false);
   // data for table
   const [tableData, setTableData] = useState([
-    {name:'Петро', address: 'Левандівка вул.', number: '+380623777778'},
-    {name:'Олег', address: 'Гіпсова вул.', number: '+38063111111'},
-    {name:'Василь', address: 'Кульпарківська вул.', number: '+38062333333'},
-    {name:'Артур', address: null, number: '+38062333333'},
-    {name:'Пантелеймон', address: null, number: null},
-    {name:'Ярослав', address: null, number: null},
-  ])
+    { name: "Петро", address: "Левандівка вул.", number: "+380623777778" },
+    { name: "Олег", address: "Гіпсова вул.", number: "+38063111111" },
+    { name: "Василь", address: "Кульпарківська вул.", number: "+38062333333" },
+    { name: "Артур", address: null, number: "+38062333333" },
+    { name: "Пантелеймон", address: null, number: null },
+    { name: "Ярослав", address: null, number: null },
+  ]);
 
   const [rows, setRows] = useState([]);
-  console.log(rows)
-
+  console.log(rows);
 
   // get data
   const getData = useCallback(async () => {
     const { data } = await axios({
-      method: 'get',
+      method: "get",
       url: `/api/ausers`,
       headers: {
-        'Content-type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
       },
     });
     const formatted = data.reduce((acc, curr) => {
-      const splitted = curr.age.split(',');
+      const splitted = curr.age.split(",");
 
       acc.push({
         id: curr.id,
@@ -189,23 +182,25 @@ export default function Carriers() {
         timezone: splitted[3],
         phone: splitted[4],
         lang: splitted[5],
-      })
+      });
       return acc;
-    },[])
+    }, []);
     setRows(formatted);
-  }, [])
+  }, []);
 
   useEffect(() => {
     getData().catch(console.error);
-  }, [getData])
+  }, [getData]);
 
   const onToggleEditMode = (id) => {
-    setRows((state) => rows.map((row) => {
-      if (row.id === id) {
-        return { ...row, isEditMode: !row.isEditMode };
-      }
-      return row;
-    }));
+    setRows((state) =>
+      rows.map((row) => {
+        if (row.id === id) {
+          return { ...row, isEditMode: !row.isEditMode };
+        }
+        return row;
+      })
+    );
   };
 
   const onChange = (e, row) => {
@@ -232,7 +227,10 @@ export default function Carriers() {
     const { id } = row;
     const newRows = rows.map((row) => {
       if (row.id === id) {
-        return { ...row, [name]: moment(value.ts ? value.ts : value).format('yyyy-MM-DD') };
+        return {
+          ...row,
+          [name]: moment(value.ts ? value.ts : value).format("yyyy-MM-DD"),
+        };
       }
       return row;
     });
@@ -244,20 +242,20 @@ export default function Carriers() {
     if (created) {
       const fetchData = async () => {
         const data = await axios({
-          method: 'post',
+          method: "post",
           url: `/api/ausers`,
           headers: {
-            'Content-type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+            "Content-type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
           },
           data: {
             age,
             name: row.name,
-          }
+          },
         });
         return data;
-      }
+      };
       const record = await fetchData().catch(console.error);
       if (record) {
         onToggleEditMode(row.id);
@@ -268,24 +266,24 @@ export default function Carriers() {
 
     const fetchData = async () => {
       await axios({
-        method: 'put',
+        method: "put",
         url: `/api/ausers`,
         headers: {
-          'Content-type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+          "Content-type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
         },
         data: {
           age,
           id: row.id,
           name: row.name,
-        }
+        },
       });
-    }
+    };
     fetchData().catch(console.error);
     onToggleEditMode(row.id);
     setCreated(false);
-  }
+  };
 
   const onRevert = (id) => {
     const newRows = rows.map((row) => {
@@ -306,12 +304,12 @@ export default function Carriers() {
     const newRows = rows.filter((row) => row.id !== id);
     setRows(newRows);
     await axios({
-      method: 'delete',
+      method: "delete",
       url: `/api/ausers/${id}`,
       headers: {
-        'Content-type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
       },
     });
     setPrevious((state) => {
@@ -322,57 +320,112 @@ export default function Carriers() {
   // from this array you can control your table like position and another ...
   // align -control position
 
-  const columns =[
-    {title: 'Назва', field: "name", align:'left',emptyValue: ()=> <em>null</em>, defaultSort: "asc"},
-    {title: 'Поштова адреса', field: "address", align:'left',emptyValue: ()=> <em>null</em>, defaultSort: "asc"},
+  const columns = [
+    {
+      title: "Назва",
+      field: "name",
+      align: "left",
+      emptyValue: () => <em>null</em>,
+      defaultSort: "asc",
+    },
+    {
+      title: "Поштова адреса",
+      field: "address",
+      align: "left",
+      emptyValue: () => <em>null</em>,
+      defaultSort: "asc",
+    },
     // if yo want to disabled for sort and search same column
     // sorting: false, searchable: false
-    {title: 'Телефон', field: "number", align:'left', emptyValue: ()=> <em>null</em>, sorting: false, searchable: false }
-  ]
-// icons
+    {
+      title: "Телефон",
+      field: "number",
+      align: "left",
+      emptyValue: () => <em>null</em>,
+      sorting: false,
+      searchable: false,
+    },
+  ];
+  // icons
   const tableIcons = {
     Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
     Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
     Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
     Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => (
+      <ChevronRight {...props} ref={ref} />
+    )),
     Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
     Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
     Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
     FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
     LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
     NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => (
+      <ChevronLeft {...props} ref={ref} />
+    )),
     ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
     Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+    SortArrow: forwardRef((props, ref) => (
+      <ArrowDownward {...props} ref={ref} />
+    )),
+    ThirdStateCheck: forwardRef((props, ref) => (
+      <Remove {...props} ref={ref} />
+    )),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
   };
 
-  
-
   return (
-    <Grid item  spacing={2} xs={12} className={classes.bg}>
-        <Paper className={classes.paper}>
-          <MaterialTable
-            title="Перевізники"
-            // columns
-            columns ={columns}
-            // icons
-            icons={tableIcons}
-            // data
-            data={tableData}
-            // in option wee add options for table like sorting ....
-            options={
-              {
-              sorting:true,
-              search: true,
-              pageSizeOptions:[5,10]
-            }
-            }
-          />
-        </Paper>
+    <Grid item spacing={2} xs={12} className={classes.bg}>
+      <Paper className={classes.paper}>
+        <MaterialTable
+          title="Перевізники"
+          // columns
+          columns={columns}
+          // icons
+          icons={tableIcons}
+          // data
+          data={tableData}
+          editable={{
+            // Callback function to add a new row to the table
+            // Крок 1: Обіцянка для асинхронного виконання операції додавання рядка
+            onRowAdd: (newRow) =>
+              new Promise((resolve, reject) => {
+                // Крок 2a: Виводимо новий рядок у консоль (опційно для налагодження)
+                // Крок 2b: Оновлюємо стан таблиці, додаючи новий рядок
+                setTableData([...tableData, newRow]);
+                console.log(newRow);
+                // Крок 2c: Викликаємо `resolve`, щоб позначити успішне виконання операції
+                setTimeout(() => resolve(), 500);
+              }),
+            onRowUpdate: (newRow, oldRow) =>
+              new Promise((resolve, reject) => {
+                const updatedDAta = [...tableData];
+                updatedDAta[oldRow.tableData.id] = newRow;
+                setTableData(updatedDAta);
+                setTimeout(() => resolve(), 500);
+              }),
+              onRowDelete: (selectedRow)=> new Promise((resolve,reject)=>{
+                const updatedDAta = [...tableData];
+                updatedDAta.splice(selectedRow.tableData.id,1)
+
+                console.log(selectedRow);
+                setTimeout(() => resolve(), 1000);
+              })
+          }}
+          // in option wee add options for table like sorting ....
+          options={{
+            sorting: true,
+            search: true,
+            pageSizeOptions: [5, 10],
+            // mod pagination
+            paginationType: "stepped",
+            showFirstLastPageButtons: false,
+            addRowPosition: "first",
+            actionsColumnIndex: -1,
+          }}
+        />
+      </Paper>
     </Grid>
   );
 }
